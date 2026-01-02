8b5f86df987bfd4e552dce399ca174ab46433282
letruxux <mrgianfranco483@gmail.com>
2026-01-02 18:27:19 +0100

    slightly faster get.ps1

--- scripts/get.ps1
+++ scripts/get.ps1
@@ -1,11 +1,15 @@
-$input | ForEach-Object {
-  Get-PnpDevice -FriendlyName "*$_*" | ForEach-Object {
-    $test = $_ |
-      Get-PnpDeviceProperty -KeyName '{104EA319-6EE2-4701-BD47-8DDBF425BBE5} 2' |
-      Where Type -ne Empty
+$pattern = ($input | ForEach-Object { [regex]::Escape($_) }) -join '|'
 
-    if ($test) {
-      $test.InstanceId
+foreach ($d in Get-PnpDevice | Where FriendlyName -match $pattern) {
+  try {
+    $p = Get-PnpDeviceProperty `
+      -InstanceId $d.InstanceId `
+      -KeyName '{104EA319-6EE2-4701-BD47-8DDBF425BBE5} 2' `
+      -ErrorAction Stop
+
+    if ($p.Type -ne 'Empty') {
+      $d.InstanceId
+      break
     }
-  }
+  } catch {}
 }
