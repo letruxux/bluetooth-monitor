8fe1d41e176621518a3ed2bc39ed37b85d038d1e
letruxux <mrgianfranco483@gmail.com>
2026-01-03 16:52:48 +0100

    and colors

--- src/constants.py
+++ src/constants.py
@@ -3,6 +3,20 @@ import subprocess as _sub
 _startupinfo = _sub.STARTUPINFO()
 _startupinfo.dwFlags |= _sub.STARTF_USESHOWWINDOW
 
+
+class _Colors:
+    red = (255, 0, 0)
+    green = (0, 255, 0)
+    blue = (0, 0, 255)
+    orange = (255, 165, 0)
+    yellow = (255, 255, 0)
+    white = (255, 255, 255)
+    black = (0, 0, 0)
+    gray = (128, 128, 128)
+
+
+colors = _Colors()
+
 APP_NAME = "BluetoothMonitor"
 SUBPROCESS_ARGS = {
     "startupinfo": _startupinfo,
